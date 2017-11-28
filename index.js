045394dc0b60f1eb785b6b549367ff96a5a43d0f
王美建 <wangmeijian@360.cn>
2017-11-28 17:53:30 +0800

    remove removeEventListener

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 'use strict';
 
 class AutoCopy {
-	constructor() {
-		this.title = document.title;
-		this.timer = null;
+    constructor() {
+        this.title = document.title;
+        this.timer = null;
 
-		this._init = this._init.bind(this);
-		this._bindEvent();
-	}
-	_init() {
-		let _this = this;
-		try {
-			// unselected
-			if (window.getSelection().toString().trim().length === 0) return;
-			// copy
-			document.execCommand('copy');
-			// prompt
-			document.title = 'copy success!';
-			_this.timer = setTimeout(function() {
-				document.title = _this.title;
-				clearTimeout(_this.timer);
-			}, 1000)
-		} catch (err) {
-			throw Error(err);
-		}
-	}
-	_bindEvent() {
-		let _this = this;
-		window.addEventListener('mouseup', _this._init, false);
-		window.addEventListener('close', function() {
-			window.removeEventListener('mouseup', _this._init, false);
-		}, false);
-	}
+        this._init = this._init.bind(this);
+        this._bindEvent();
+    }
+    _init() {
+        let _this = this;
+        try {
+            // unselected
+            if (
+                window
+                    .getSelection()
+                    .toString()
+                    .trim().length === 0
+            )
+                return;
+            // copy
+            document.execCommand('copy');
+            // prompt
+            document.title = 'copy success!';
+            _this.timer = setTimeout(function() {
+                document.title = _this.title;
+                clearTimeout(_this.timer);
+            }, 1000);
+        } catch (err) {
+            throw Error(err);
+        }
+    }
+    _bindEvent() {
+        window.addEventListener('mouseup', this._init, false);
+    }
 }
 
 module.exports = AutoCopy;
